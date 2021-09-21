e1d5aed8632d56d979305819a1440bdd741aa255
Guleri24 <guleri24@tuta.io>
2021-09-21 21:48:24 +0530

    Reworked on mood.py - added some examples - renamed argument option mood to enable - about section added

--- mood.py
+++ mood.py
@@ -1,23 +1,23 @@
-# #!/usr/bin/env python
-
 import os
 import argparse
 
-# Enter your moods and commands
+# Browser (make sure to add space after browser command)
 browser = "firefox "
 
+# Websites
+eclipse_foundation = browser + "https://blogs.eclipse.org/"
+easter_egg = browser + "https://youtu.be/DGa6MAibjzA"
+instagram = browser + "https://www.instagram.com/"
+java_magazine = browser + "https://blogs.oracle.com/javamagazine/"
+linkedin = browser + "https://www.linkedin.com/feed/"
+phoronix = browser + "https://www.phoronix.com/scan.php?page=home"
 
-google = browser + "www.google.com/"
-
-# Moods
-def hello(args):
-	print('Use -h for help, {0}!'.format(args.name))
-
-def mood(args):
-	if args.mood == 'google':
-		os.system(google)
-	else:
-		os.system(browser + args.mood)
+# Applications
+spotify = ""
+intellij_idea = ""
+eclipse = ""
+signal = ""
+discord = ""
 
 def add(args):
 	os.system("nano mood.py")
@@ -25,22 +25,64 @@ def add(args):
 def delete(args):
 	os.system("nano mood.py")
 
-def usage():
-	print("ggs")
+# Make sure to add new moods
+def updates():
+	print("Available moods:")
+	print("updates, social, coding \n")
+	print("Example: python mood.py enable updates")
 
-# Initalize parser
+def about():
+	print("""
+
+                        _
+  /\/\   ___   ___   __| |
+ /    \ / _ \ / _ \ / _` |
+/ /\/\ \ (_) | (_) | (_| |
+\/    \/\___/ \___/ \__,_|
+
+Minimalistic user mood generator and executor
+    """)
+
+# Add you moods here
+def mood(args):
+    if args.mood == 'updates':
+        os.system(java_magazine)
+        os.system(phoronix)
+        os.system(eclipse_foundation)
+
+    elif args.mood == 'social':
+	    os.system(instagram)
+	    os.system(linkedin)
+
+    elif args.mood == 'coding':
+	    print("coding..")
+
+    else:
+	    os.system(easter_egg)
+
+
+
+
+# Initialize parser
 parser = argparse.ArgumentParser()
 subparsers= parser.add_subparsers()
 
-mood_parser = subparsers.add_parser('mood')
+mood_parser = subparsers.add_parser('enable', help="enter desired mood")
 mood_parser.add_argument('mood')
 mood_parser.set_defaults(func=mood)
 
+add_mood = subparsers.add_parser('add', help="add new mood")
+add_mood.set_defaults(func=add)
+
+delete_mood = subparsers.add_parser('delete', help="delete mood")
+delete_mood.set_defaults(func=delete)
 
 if __name__ == '__main__':
 	args = parser.parse_args()
 	try:
 		args.func(args)
 	except AttributeError:
-		usage()
+		about()
+		updates()
+		print("no argument passed, use -h for help")
 		parser.exit()
